5176cd6e744278ca62e029e4556c0a608b23aa4a
Diana-Bernardos <dianilla75@gmail.com>
2025-02-07 18:58:14 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      animation: {
+        'bounce': 'bounce 1s infinite',
+        'fade-in': 'fadeIn 1s ease-in',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        }
+      }
+    }
+  },
+  plugins: []
+}
